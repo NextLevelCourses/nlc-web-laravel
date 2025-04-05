531d46cd44caea157751e6c99cce4be73db14d51
teguhafrianda <teguhafrianda8@gmail.com>
2025-04-05 20:14:40 +0700

    Resolve conflict in routes/web.php on development branch

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
-use App\Http\Controllers\BannerController;
+use App\Http\Controllers\CourseLayoutController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('master');
 });
 
-Route::get('/banner', [BannerController::class, 'index'])->name('banner.index');
+Route::get('/Dashboard', [DashboardController::class, 'index'])->name('Dashboard.index');
+Route::get('/CourseLayout', [CourseLayoutController::class, 'index'])->name('CourseLayout.index');
