bc1d26f06670c0520a2defe7d091db21051c3b28
teguhafrianda <teguhafrianda8@gmail.com>
2025-04-06 17:39:17 +0700

    landing page and dashboard master

--- resources/views/landing/home.blade.php
+++ resources/views/landing/home.blade.php
@@ -1,6 +1,6 @@
 @extends('landing.layouts.app')
 
-@section('title', 'Dashboard')
+@section('title', 'Landing Page')
 
 @section('content')
 <!-- Mobile Menu Section -->
@@ -2377,63 +2377,63 @@
                             </ul>
 
                             <div class="contact-btn mt--30">
-    <a class="rbt-btn hover-icon-reverse btn-border-gradient radius-round" href="/contact">
-        <div class="icon-reverse-wrapper">
-            <span class="btn-text">Contact Next Level</span>
-            <span class="btn-icon"><i class="feather-arrow-right"></i></span>
-            <span class="btn-icon"><i class="feather-arrow-right"></i></span>
+                                <a class="rbt-btn hover-icon-reverse btn-border-gradient radius-round" href="/contact">
+                                    <div class="icon-reverse-wrapper">
+                                        <span class="btn-text">Contact Next Level</span>
+                                        <span class="btn-icon"><i class="feather-arrow-right"></i></span>
+                                        <span class="btn-icon"><i class="feather-arrow-right"></i></span>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Bagian Links -->
+                    <div class="col-lg-2 col-md-6 col-sm-6 col-12">
+                        <div class="footer-widget">
+                            <h5 class="ft-title">Next Level</h5>
+                            <ul class="ft-link">
+                                <li><a href="/courses">Courses</a></li>
+                                <li><a href="/mentoring-projects">Mentoring Projects</a></li>
+                                <li><a href="/private-classes">Private Classes</a></li>
+                                <li><a href="/about-us">About Us</a></li>
+                                <li><a href="/faqs">FAQ</a></li>
+                                <li><a href="/privacy-policy">Privacy Policy</a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <!-- Bagian Company -->
+                    <div class="col-lg-2 col-md-6 col-sm-6 col-12">
+                        <div class="footer-widget">
+                            <h5 class="ft-title">For Members</h5>
+                            <ul class="ft-link">
+                                <li><a href="/login">Login</a></li>
+                                <li><a href="/register">Register</a></li>
+                                <li><a href="/my-courses">My Courses</a></li>
+                                <li><a href="/dashboard">Dashboard</a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <!-- Bagian Kontak -->
+                    <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+                        <div class="footer-widget">
+                            <h5 class="ft-title">Get in Touch</h5>
+                            <ul class="ft-link">
+                                <li><span>Phone:</span> <a href="#">+62 812-3456-7890</a></li>
+                                <li><span>Email:</span> <a href="mailto:admin@nextlevelcourse.com">admin@nextlevelcourse.com</a></li>
+                            </ul>
+
+                            <form class="newsletter-form mt--20" action="#">
+                                <h6 class="w-600">Full Address</h6>
+                                <p class="description">Next Level Course HQ<br />Jl. Belajar No. 123, Bandung, Indonesia</p>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    </a>
-</div>
-</div>
-</div>
-
-<!-- Bagian Links -->
-<div class="col-lg-2 col-md-6 col-sm-6 col-12">
-    <div class="footer-widget">
-        <h5 class="ft-title">Next Level</h5>
-        <ul class="ft-link">
-            <li><a href="/courses">Courses</a></li>
-            <li><a href="/mentoring-projects">Mentoring Projects</a></li>
-            <li><a href="/private-classes">Private Classes</a></li>
-            <li><a href="/about-us">About Us</a></li>
-            <li><a href="/faqs">FAQ</a></li>
-            <li><a href="/privacy-policy">Privacy Policy</a></li>
-        </ul>
-    </div>
-</div>
-
-<!-- Bagian Company -->
-<div class="col-lg-2 col-md-6 col-sm-6 col-12">
-    <div class="footer-widget">
-        <h5 class="ft-title">For Members</h5>
-        <ul class="ft-link">
-            <li><a href="/login">Login</a></li>
-            <li><a href="/register">Register</a></li>
-            <li><a href="/my-courses">My Courses</a></li>
-            <li><a href="/dashboard">Dashboard</a></li>
-        </ul>
-    </div>
-</div>
-
-<!-- Bagian Kontak -->
-<div class="col-lg-4 col-md-6 col-sm-6 col-12">
-    <div class="footer-widget">
-        <h5 class="ft-title">Get in Touch</h5>
-        <ul class="ft-link">
-            <li><span>Phone:</span> <a href="#">+62 812-3456-7890</a></li>
-            <li><span>Email:</span> <a href="mailto:admin@nextlevelcourse.com">admin@nextlevelcourse.com</a></li>
-        </ul>
-
-        <form class="newsletter-form mt--20" action="#">
-            <h6 class="w-600">Full Address</h6>
-            <p class="description">Next Level Course HQ<br />Jl. Belajar No. 123, Bandung, Indonesia</p>
-        </form>
-    </div>
-</div>
-</div>
-</div>
-</div>
 
 <!-- Copyright -->
 <div class="rbt-separator-mid">
@@ -2446,7 +2446,7 @@
     <div class="container">
         <div class="row align-items-center">
             <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-12 col-12">
-                <p class="rbt-link-hover text-center text-lg-start">Copyright © 2025 <a href="#">Next Level Course</a>. All Rights Reserved.</p>
+                <p class="rbt-link-hover text-center text-lg-start">Copyright © 2025 <a href="#">Next Level Course</a> All Rights Reserved.</p>
             </div>
             <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-12 col-12">
                 <ul class="copyright-link rbt-link-hover justify-content-center justify-content-lg-end mt_sm--10 mt_md--10">
