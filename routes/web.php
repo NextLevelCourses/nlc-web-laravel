7dcd1a67af260d8228e5a610a629566f05c2a94c
teguhafrianda <teguhafrianda8@gmail.com>
2025-04-06 19:32:43 +0700

    implement instructor and student dashboards with routing and controller setup

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [LandingController::class, 'index']);
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('/instructor', [DashboardController::class, 'instructor'])->name('instructor');
+Route::get('/student', [DashboardController::class, 'student'])->name('student');
+
 
