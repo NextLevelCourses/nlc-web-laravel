cbb42850d39fac5c586cc0daf26e2276facada82
teguhafrianda <teguhafrianda8@gmail.com>
2025-04-07 11:22:26 +0700

    Feature/landing page structure (#18)
    * base database
    
    * add item platform nabar
    
    * improve header navbar layout for special programs and courses
    
    * finish landing page with hero section, course categories, and fake testimonials fake
    
    * fix structur folder landing page
    
    * setup landing page structure for guest users
    
    * landing page and dashboard master

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\LandingController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [LandingController::class, 'index']);
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
