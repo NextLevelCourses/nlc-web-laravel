490b642fb4b4e462130d259f8c541bc78a7c5f51
teguhafrianda <teguhafrianda8@gmail.com>
2025-04-15 21:22:38 +0700

    Sejajara

--- resources/views/dashboard/module/CreateCourse.blade.php
+++ resources/views/dashboard/module/CreateCourse.blade.php
@@ -861,46 +861,61 @@
             </div>
         </div>
 
+            <!-- Start Modal Area -->
+        <div class="rbt-default-modal modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
 
-      <!-- Start Modal Area -->
-    <div class="rbt-default-modal modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="rbt-round-btn btn-close" data-bs-dismiss="modal" aria-label="Close">
-                        <i class="feather-x"></i>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <div class="inner rbt-default-form">
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <h5 class="modal-title mb--20" id="exampleModalLabel">Add Topic</h5>
-                                <div class="course-field mb--20">
-                                    <label for="modal-field-1">Topic Name</label>
-                                    <input id="modal-field-1" type="text" class="form-control">
-                                    <small><i class="feather-info"></i> Topic titles are displayed publicly wherever required. Each topic may contain one or more lessons, quiz and assignments.</small>
-                                </div>
-                                <div class="course-field mb--20">
-                                    <label for="modal-field-2">Topic Summary</label>
-                                    <textarea id="modal-field-2" class="form-control"></textarea>
-                                    <small><i class="feather-info"></i> Add a summary of short text to prepare students for the activities for the topic. The text is shown on the course page beside the tooltip beside the topic name.</small>
+                    <div class="modal-header">
+                        <button type="button" class="rbt-round-btn btn-close" data-bs-dismiss="modal" aria-label="Close">
+                            <i class="feather-x"></i>
+                        </button>
+                    </div>
+
+                    <div class="modal-body">
+                        <div class="inner rbt-default-form">
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <h5 class="modal-title mb--20" id="exampleModalLabel">Add Topic</h5>
+
+                                    <!-- Topic Name -->
+                                    <div class="course-field mb--20">
+                                        <label for="modal-field-1">Topic Name</label>
+                                        <input id="modal-field-1" type="text" class="form-control">
+                                        <small><i class="feather-info"></i> Topic titles are displayed publicly wherever required. Each topic may contain one or more lessons, quiz and assignments.</small>
+                                    </div>
+
+                                    <!-- Topic Summary -->
+                                    <div class="course-field mb--20">
+                                        <label for="modal-field-2">Topic Summary</label>
+                                        <textarea id="modal-field-2" class="form-control"></textarea>
+                                        <small><i class="feather-info"></i> Add a summary of short text to prepare students for the activities for the topic. The text is shown on the course page beside the tooltip beside the topic name.</small>
+                                    </div>
+
+                                    <!-- Enable Cart (Checkbox) -->
+                                    <div class="course-field mb--20">
+                                        <div class="form-check d-flex align-items-center">
+                                            <input class="form-check-input me-2" type="checkbox" id="rbt-checkbox-11" name="rbt-checkbox-11" value="yes">
+                                            <label class="form-check-label mb-0" for="rbt-checkbox-11">Enable Cart</label>
+                                        </div>
+                                    </div>
+
                                 </div>
                             </div>
-                        </div>e 
+                        </div>
                     </div>
-                </div>
-                <div class="course-field mb--20">
-                    <p class="rbt-checkbox-wrapper mb--5 d-flex"><input id="rbt-checkbox-11" name="rbt-checkbox-11" type="checkbox" value="yes"><label for="rbt-checkbox-11">Enable Cart</label></p>
-                </div>
-                <div class="modal-footer pt--30">
-                    <button type="button" class="rbt-btn btn-border btn-md radius-round-10 btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                    <button type="button" class="rbt-btn btn-gradient btn-md radius-round-10 btn btn-primary" data-bs-dismiss="modal">OK</button>
+
+                    <!-- Modal Footer -->
+                    <div class="modal-footer pt--30">
+                        <button type="button" class="rbt-btn btn-border btn-md radius-round-10 btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                        <button type="button" class="rbt-btn btn-gradient btn-md radius-round-10 btn btn-primary" data-bs-dismiss="modal">OK</button>
+                    </div>
+
                 </div>
             </div>
         </div>
-    </div>
-        <!-- End Modal Area  -->
+        <!-- End Modal Area -->
+
 
         <!-- Start Topic Area  -->
         <div class="rbt-default-modal modal fade" id="UpdateTopic" tabindex="-1" aria-labelledby="UpdateTopicLabel" aria-hidden="true">
