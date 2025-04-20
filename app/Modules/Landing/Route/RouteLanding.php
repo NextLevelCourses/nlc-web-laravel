bb8054d8d63ef5b6cd9f303d430d8db6aa8083b1
xamppsul <samsul.marif100@gmail.com>
2025-04-20 11:42:00 +0800

    forgot password update

--- app/Modules/Landing/Route/RouteLanding.php
+++ app/Modules/Landing/Route/RouteLanding.php
@@ -9,6 +9,7 @@ Route::get('/', [HandlerLanding::class, 'Index'])->name('landing.Home');
 Route::get('/About', [HandlerLanding::class, 'About'])->name('landing.About');
 Route::get('/Testimonial', [HandlerLanding::class, 'Testimonial'])->name('landing.Testimonial');
 Route::get('/Authentication', [HandlerLanding::class, 'Authentication'])->name('landing.Authentication');
+Route::get('/ResetPassword', [HandlerLanding::class, 'ResetPassword'])->name('landing.ResetPassword');
 Route::get('/FAQs', [HandlerLanding::class, 'FAQs'])->name('landing.FAQs');
 Route::get('/DetailBlog', [HandlerLanding::class, 'DetailBlog'])->name('landing.DetailBlog');
 Route::get('/DetailCourseLanding', [HandlerLanding::class, 'DetailCourseLanding'])->name('landing.DetailCourseLanding');
